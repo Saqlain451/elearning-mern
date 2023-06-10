bb979f5e95a28b796094573916bcacacb50d462a
Saqlain451 <jobs.sakil.mustak@gmail.com>
2023-06-10 11:16:38 +0530

    video comp updated

--- client/src/Pages/CourseVideos/CourseVideos.jsx
+++ client/src/Pages/CourseVideos/CourseVideos.jsx
@@ -1,6 +1,18 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useGloblaHook } from '../../Hooks/Context'
+import { useParams } from 'react-router-dom';
 
 const CourseVideos = () => {
+  const {getInsApiData, url} = useGloblaHook();
+  const [videodata, setVideoData] = useState({})
+  
+  const params = useParams();
+  const courseId = params.id;
+  console.log(courseId);
+  useEffect(()=>{
+    getInsApiData(`${url}/courses/${courseId}`,setVideoData)
+  },[])
+  console.log(videodata);
   return (
     <div>CourseVideos</div>
   )
